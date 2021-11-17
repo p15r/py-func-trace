cd554454e6f26806aaab48e4ab708eb93e9a1656
pat <pat@p15r.net>
2021-11-17 07:32:40 +0000

    feat: add test that captures "print()" to stdout

--- tests/test_all.py
+++ tests/test_all.py
@@ -3,6 +3,7 @@
 import logging
 import inspect
 import sys
+import io
 from typing import Dict
 from unittest import TestCase
 
@@ -20,7 +21,7 @@ class TestAll(TestCase):
         """Testing all functionality in one go."""
         func_trace.enter(inspect.currentframe())
 
-        print(f"i: {i}, a_string:{a_string}, priv_foo: {priv_foo}, dic: {dic}")
+        print(f"i: {i}, a_string: {a_string}, priv_foo: {priv_foo},dic: {dic}")
 
         ret = {"a": "s", "priv_b": "secret"}
         func_trace.leave(inspect.currentframe(), ret)
@@ -28,6 +29,18 @@ class TestAll(TestCase):
 
     def test_is_string(self):
         """Tests all_in_one()."""
-        # TODO: add moar tests (also parse stdoutput and check for camouflage
+        # TODO: test that captures logging (trace) info, not only prints
+        #       redirect logging to stdout for this?
         ret = self.all_in_one(5, "b", 123, {"foo": "s", "priv_bar": "secret"})
         self.assertTrue(isinstance(ret, dict))
+
+    def test_stdout(self):
+        """to be done"""
+        expected_stdout = ("i: 5, a_string: b, priv_foo: 123,"
+                           "dic: {'foo': 's', 'priv_bar': 'secret'}")
+
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        self.all_in_one(5, "b", 123, {"foo": "s", "priv_bar": "secret"})
+        sys.stdout = sys.__stdout__
+        self.assertEqual(captured_output.getvalue().strip(), expected_stdout)
