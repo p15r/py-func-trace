ee987c28db2afd19990afd5bc5de6c18c56e0105
pat <pat@p15r.net>
2021-11-17 20:44:21 +0000

    feat: add tests for print and logging output

--- tests/test_all.py
+++ tests/test_all.py
@@ -5,7 +5,7 @@ import inspect
 import sys
 import io
 from typing import Dict
-from unittest import TestCase
+from unittest import TestCase, mock
 
 from py_func_trace import func_trace
 
@@ -15,7 +15,7 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 class TestAll(TestCase):
     """Test class for py-func-trace"""
     @staticmethod
-    def all_in_one(
+    def fixture_func(
         i: int, a_string: str, priv_foo: int, dic: Dict[str, str]
     ) -> Dict[str, str]:
         """Testing all functionality in one go."""
@@ -28,19 +28,46 @@ class TestAll(TestCase):
         return ret
 
     def test_is_string(self):
-        """Tests all_in_one()."""
-        # TODO: test that captures logging (trace) info, not only prints
-        #       redirect logging to stdout for this?
-        ret = self.all_in_one(5, "b", 123, {"foo": "s", "priv_bar": "secret"})
+        """Tests return value"""
+        ret = self.fixture_func(
+            5, "b", 123, {"foo": "s", "priv_bar": "secret"}
+        )
         self.assertTrue(isinstance(ret, dict))
 
-    def test_stdout(self):
-        """to be done"""
+    def test_print(self):
+        """Test print output"""
         expected_stdout = ("i: 5, a_string: b, priv_foo: 123,"
                            "dic: {'foo': 's', 'priv_bar': 'secret'}")
 
         captured_output = io.StringIO()
         sys.stdout = captured_output
-        self.all_in_one(5, "b", 123, {"foo": "s", "priv_bar": "secret"})
+        self.fixture_func(5, "b", 123, {"foo": "s", "priv_bar": "secret"})
         sys.stdout = sys.__stdout__
         self.assertEqual(captured_output.getvalue().strip(), expected_stdout)
+
+    def test_trace(self):
+        """Test logging output"""
+        expected_output = []
+        expected_output.append("call('(%s:%s) Entering \"%s\" args: %s', "
+                               "'test_all.py', 17, 'fixture_func', {'i': 5, "
+                               "'a_string': 'b', 'priv_foo': '******', "
+                               "'dic': {'foo': 's', 'priv_bar': '******'}})")
+
+        expected_output.append("call('(%s:%s) Exiting \"%s\" ret: %s', "
+                               "'test_all.py', 17, 'fixture_func', {'a': "
+                               "'s', 'priv_b': 'secret'})")
+
+        logger = logging.getLogger("py_func_trace.func_trace")
+        with mock.patch.object(logger, 'info') as mock_info:
+            self.fixture_func(5, "b", 123, {"foo": "s", "priv_bar": "secret"})
+            print(dir(mock_info))
+            print(mock_info.mock_calls[1].__str__())
+            mock_info.assert_called()
+            self.assertEqual(
+                expected_output[0],
+                mock_info.mock_calls[0].__str__()
+            )
+            self.assertEqual(
+                expected_output[1],
+                mock_info.mock_calls[1].__str__()
+            )
